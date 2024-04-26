b28eb5725a35b3881503b415070d40617e0c60f9
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-04-26 13:56:30 -0300

    Create Autentication and Login

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -35,6 +35,74 @@ router.post("/register", async (req, res) => {
     const passwordhash = await bcrypt.hash(password, salt)
     console.log(passwordhash);
 
+    const user = new User({
+        name: name,
+        email: email,
+        password: passwordhash
+    })
+
+    try {
+        
+        const newUser = await user.save()
+
+        // create token 
+        const token = jwt.sign(
+            
+            //payload
+            {
+                name: newUser.name,
+                id: newUser._id
+            },
+            "nossoscret"
+
+        )
+
+        // return token
+        res.json({error: null, msg:"Você realizou o cadastro com sucesso", token: token, userId: newUser._id})
+
+    } catch (error) {
+        
+        res.status(400).json({error})
+
+    }
+
+})
+
+// login an user
+router.post("/login", async (req, res) => {
+
+    const email = req.body.email
+    const password = req.body.password
+
+    // check if  user exists
+    const user = await User.findOne({email: email})
+
+    if(!user){
+        return res.status(400).json({ error: "Não há usuário cadastrado com esse e-mail!"})
+    }
+
+    // check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword){
+        return res.status(400).json({ error: "Senha inválida!"})
+    }
+
+    // create token 
+    const token = jwt.sign(
+    
+        //payload
+        {
+            name: user.name,
+            id: user._id
+        },
+        "nossoscret"
+
+    )
+
+    // return token
+    res.json({error: null, msg:"Você está autenticado!", token: token, userId: user._id})
+
 })
 
 module.exports = router
