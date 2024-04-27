7db538c3109b9a4b38dbb3c92bc02e1469de1037
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-04-27 09:25:35 -0300

    today deceptions

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,6 +7,9 @@ const User =  require("../models/user")
 // middlewares
 const verifyToken = require("../helpers/check-token")
 
+// helpers
+const getUserByToken = require("../helpers/get-user-by-token")
+
 // get an user for id
 router.get("/:id", verifyToken, async (req, res) => {
 
@@ -29,6 +32,60 @@ router.get("/:id", verifyToken, async (req, res) => {
 
 // update an user
 router.put("/", verifyToken, async (req, res) => {
+
+    const token = req.header("auth-token")
+    const user = await getUserByToken(token)
+    const userReqId = req.body.id
+    const password = req.body.password
+    const confirmpassword = req.body.confirmpassword
+
+    const userId = user._id.toString()
+
+    // check if user id is equal token user id
+    if(userId != userReqId){
+
+        res.status(401).json({ error: "Acessso negado"})
+        
+    }
+
+    // create an user object
+    const updateData = {
+
+        nome: req.body.name,
+        email: req.body.email
+        
+    }
+
+    // check if password match
+    if(password != confirmpassword){
+
+        res.status(401).json({error: "As senhas não coincidem"})
+        //change password
+
+    } else if(password == confirmpassword && password != null){
+
+        // creating password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        //add password to data
+        updateData.password = passwordHash
+
+    }
+
+
+    try {
+
+        // returns updated data
+        const updateUser = await User.findOneAndUpdate({_id: userId}, {$set: updateData}, {new: true})
+        res.json({ error: null, msg: "usuárion atualizado com sucesso!", data: updateUser})
+        
+    } catch (error) {
+
+        res.status(400).json({ error})
+
+    }
+
 
 })
 
