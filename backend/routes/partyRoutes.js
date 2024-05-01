d0a83b51a79a6467d364b0924db8e5048f2275eb
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-04-30 22:50:24 -0300

    created files to party

---
+++ backend/routes/partyRoutes.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const router = express.Router()
+const jwt = require("jsonwebtoken")
+const multer = require("multer") //biblioteca para fazer o upload de imagens
+
+const Party = require("../models/party")
+const User = require("../models/user")
+
+// define file storage
+
+
+// middlewares
+const verifyToken = require("../helpers/check-token")
+
+// helpers 
+const getUserByToken =  require("../helpers/get-user-by-token")
+
+// get party
+
+router.get("/", verifyToken, (req, res) => [
+
+    res.json({ msg:  "funcionando gay" })
+
+])
+
+module.exports = router
--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const cors = require("cors")
 // routes
 const authRouter = require("./routes/authRoutes.js")
 const userRouter = require("./routes/userRoutes.js")
+// const partyRouter = require("./routes/partyRoutes.js")
 
 
 // middlewares
@@ -24,6 +25,7 @@ app.use(express.static('public'))
 //atrelar as rotas no express
 app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
+// app.use("/api/party", partyRouter)
 
 // Conexão mongodb
 mongoose.connect(`mongodb://127.0.0.1/${dbname}`)
