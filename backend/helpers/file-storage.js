d2f8c1728ee945e6bd46d88957f3572bf677117a
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-04-30 23:25:16 -0300

    upload settings Sended

---
+++ backend/helpers/file-storage.js
@@ -0,0 +1,15 @@
+const multer = require("multer");
+const path = require("path")
+
+const diskStorage = multer.diskStorage({
+
+    destination: (req, res, cb) => { // onde os arquivos serão salvos e qual o padrão de nomes
+        cb(null, 'pulic/img')
+    },
+    filename: (req, file, cb) => {   
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+
+})
+
+module.exports = diskStorage
--- backend/routes/partyRoutes.js
+++ backend/routes/partyRoutes.js
@@ -7,7 +7,8 @@ const Party = require("../models/party")
 const User = require("../models/user")
 
 // define file storage
-
+const diskStorage = require("../helpers/file-storage")
+const upload = multer({ storage: diskStorage })
 
 // middlewares
 const verifyToken = require("../helpers/check-token")
