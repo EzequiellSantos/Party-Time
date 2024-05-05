7ab14497595c46efb0770d890f180e47bd502eee
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-05-05 15:10:14 -0300

    Views Authenticated Updated

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -6,10 +6,21 @@
         </router-link>
         <h2 id="nav-title">Party Time</h2>
 
-        <div id="nav-links">
-            <router-link to="/">Home</router-link>
-            <router-link to="/about">About</router-link>
-            <router-link to="/register">Cadastrar</router-link>
+        <div id="nav-links" ref="links">
+            <router-link to="/" class="links-menu">Home</router-link>
+            <router-link to="/about" class="links-menu" v-show="!authenticated">Entrar</router-link>
+            <router-link to="/register" class="links-menu" v-show="!authenticated">Cadastrar</router-link>
+            <router-link to="/dashboard" class="links-menu" v-show="authenticated">Dashboard</router-link>
+            <router-link to="/profile" class="links-menu" v-show="authenticated">Configuração</router-link>
+        </div>
+
+        <button @click="logout($event)" v-show="authenticated">
+            <img width="48" height="36" src="https://img.icons8.com/sf-regular/48/exit.png" alt="exit" />
+        </button>
+
+        <div id="menu" @click="clicou">
+            <img id="iconMenuHambuger" ref="iconMenuHambuger" width="48" height="38" src="https://img.icons8.com/sf-regular/48/menu.png" alt="menu"/>
+            <img ref="iconMenuArrow" id="iconMenuArrow" width="48" height="38" src="https://img.icons8.com/sf-regular/48/collapse-arrow.png" alt="collapse-arrow"/>
         </div>
 
     </div>
@@ -17,8 +28,45 @@
 
 <script>
 
+    import { mapState } from 'vuex'
+
     export default ({
-        name: "Navbar"
+        name: "Navbar",
+        methods: {
+            clicou(){
+                const links = this.$refs.links
+                const iconMenuArrow = this.$refs.iconMenuArrow
+                const iconMenuHambuger = this.$refs.iconMenuHambuger
+
+                if(links.style.display == 'block' && window.innerWidth < 750){
+                    links.style.display = 'none'
+                    iconMenuHambuger.style.display = 'block'
+                    iconMenuArrow.style.display = 'none'
+                } else if(window.innerWidth < 750){
+                    links.style.display = 'block'
+                    iconMenuHambuger.style.display = 'none'
+                    iconMenuArrow.style.display = 'block'
+                }
+                    
+            },
+            logout(e){
+
+                e.preventDefault()
+            
+                // emit event for auth an user
+                this.$store.commit('logout') //enviando os dados para o state() ./store
+
+                // redirect
+                this.$router.push('/')
+
+            }
+
+        },
+        computed: { // quando acomtecer alguma alteração na págoina ele será chamado
+            ...mapState([
+                'authenticated'
+            ])
+        }       
     })
 
 </script>
@@ -26,7 +74,8 @@
 
 <style scoped>
 
-    #nav{
+    #nav{ 
+        width: 100%;
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -39,16 +88,30 @@
         margin-right: 15px;
     }
 
+    #menu{
+        display: none;
+        width: 48px;
+        height: 38px;
+        display: flex;
+        align-content: center;
+        justify-content: center;
+    }
+
+    #iconMenuHambuger, #iconMenuArrow{
+        display: none;
+    }
+
     #nav img{
         width: 35px;
     }
 
     #nav-title {
         font-size: 3rem;
-        font-weight: 300;
+        font-weight: 300;  
+        text-align: center; 
     }
 
-    #logo-container, #nav-links {
+    #logo-container, #nav-links, #nav-title {
         width: 400px;
     } 
 
@@ -66,6 +129,51 @@
 
     button:is(:hover, :focus){
         color: #c1b696;
+    }
+
+    @media (750px > width) {
+        
+        #nav-title{
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            width: 200px;
+            margin: auto;
+            text-align: center;
+            font-size: clamp(1.75rem, 1.3712rem + 0.6061vw, 1.8rem);
+        }
+
+        #nav-links{
+            display: none;
+            position: absolute;
+            top: 75px;
+            left: 51%;
+            transform: translateX(-50%);
+            width: auto;
+        }
+        
+        #menu{
+            display: block;
+            cursor: default;
+            width: 48px;
+        }
+
+        #iconMenuArrow{
+            display: none;
+        }
+
+        #iconMenuHambuger{
+            display: block;
+        }
+
+        .links-menu{
+
+        }
+
+        #logo-container{
+            width: 30px;
+        }
+
     }
 
 </style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="login">
+        <h1>Login</h1>
+        <p>prossiga com o login para desfrutar melhor da aplicação</p>
+        <form></form>
+    </div>
+</template>
+
+<style>
+    #login{
+        text-align: center;
+    }
+</style>
