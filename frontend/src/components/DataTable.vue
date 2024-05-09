1020cd060086c3f078ec4282547566b3ff3ce73f
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-05-09 08:35:37 -0300

    Component DataTable Created

---
+++ frontend/src/components/DataTable.vue
@@ -0,0 +1,22 @@
+<template>
+
+    <div class="data-table">
+
+        <h1>Aiii calica</h1>
+
+
+    </div>
+
+</template>
+
+<script>
+
+    export default {
+        name: "DataTable"
+    }
+
+</script>
+
+<style scoped>
+
+</style>
