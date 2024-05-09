7ae4b7d20597ad8a144a0aed7e64c2af8f747b6b
Ezequiel Santos <111365548+EzequiellSantos@users.noreply.github.com>
2024-05-09 07:42:37 -0300

    fixes path

--- backend/helpers/file-storage.js
+++ backend/helpers/file-storage.js
@@ -4,7 +4,7 @@ const path = require("path")
 const diskStorage = multer.diskStorage({
 
     destination: (req, res, cb) => { // onde os arquivos serão salvos e qual o padrão de nomes
-        cb(null, 'pulic/img')
+        cb(null, 'public/img/')
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname))
